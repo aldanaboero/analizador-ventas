9ee4965ffd2b975f6d6ebf5609390bdf0329dd62
aldanaboero <106601499+aldanaboero@users.noreply.github.com>
2026-03-06 09:17:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Analizador de Ventas")
+st.title("📊 Analizador de Ventas")
 
 file = st.file_uploader("Sube un archivo CSV o Excel", type=["csv","xlsx"])
 
@@ -12,5 +12,14 @@ if file is not None:
     else:
         df = pd.read_excel(file)
 
-    st.write("Datos cargados:")
+    st.subheader("📋 Datos del archivo")
     st.dataframe(df)
+
+    st.subheader("📈 Estadísticas")
+    st.write(df.describe())
+
+    st.subheader("📊 Gráfico de ventas")
+
+    if "Producto" in df.columns and "Total" in df.columns:
+        ventas = df.groupby("Producto")["Total"].sum()
+        st.bar_chart(ventas)
