5ec2cbd1d4386ac15a62cf250308c05d75623b35
aldanaboero <106601499+aldanaboero@users.noreply.github.com>
2026-03-06 09:10:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ import pandas as pd
 
 st.title("Analizador de Ventas")
 
-file = st.file_uploader("Sube un archivo Excel o CSV")
+file = st.file_uploader("Sube un archivo CSV o Excel", type=["csv","xlsx"])
 
-if file:
-    df = pd.read_csv(file)
-    
+if file is not None:
+
+    if file.name.endswith(".csv"):
+        df = pd.read_csv(file, encoding="latin1")
+    else:
+        df = pd.read_excel(file)
+
     st.write("Datos cargados:")
     st.dataframe(df)
-
-    st.subheader("Gráfico automático")
-    st.line_chart(df)
